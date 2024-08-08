197637b65cc8f7cb7bb2ce256659f65a2a242aa2
GrzegorzStacel <stacelgrzegorz@gmail.com>
2024-08-08 19:36:12 +0200

    Refactor imports from CommonJS to ESM

--- helpers/modifyJsonFile.js
+++ helpers/modifyJsonFile.js
@@ -1,9 +1,14 @@
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
 import { getFormattedActualDate } from "./getFormattedActualDate.js";
 
 export async function modifyJsonFile(filePathArg, indexActualArray, isSafe) {
   try {
+    // Uzyskanie ścieżki do katalogu roboczego
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
     // Ścieżka do pliku JSON
     const filePath = path.join(__dirname, filePathArg);
 
