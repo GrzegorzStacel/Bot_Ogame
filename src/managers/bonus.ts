bbe0818dccde28501f635b3f2f5ca182ed730d5a
GrzegorzStacel <stacelgrzegorz@gmail.com>
2024-08-15 13:11:56 +0200

    Refactoring

--- src/managers/bonus.ts
+++ src/managers/bonus.ts
@@ -1,27 +1,23 @@
-import puppeteer from "puppeteer";
+import { ElementHandle } from "puppeteer";
 import { setupBrowser } from "../setupBrowser/setupBrowser.js";
 import { delay } from "../utils/delay.js";
-import { takeInnerText } from "../helpers/takeInnerText.js";
 
-export async function bonus() {
+export async function bonus(): Promise<void> {
   const { page } = await setupBrowser();
 
-  // TODO jeśli można głosować - uruchomić sygnał dźwiękowy
-  // TODO zablokować inne funkcje!
-
   try {
-    const bonusButton = await page.$("#btn-online-bonus");
+    const bonusButton: ElementHandle<Element> = await page.$("#btn-online-bonus");
 
     if (bonusButton) {
       await bonusButton.click();
+
       await delay(2000);
-      console.log("Znaleziono bonus!");
+
+      console.log("Otrzymano bonus!");
     } else {
-      console.log("Brak bonusa :(");
+      console.log("Brak bonusa.");
     }
   } catch (error) {
     console.error("Wystąpił błąd w bonus.js:", error);
   }
-
-  await delay(3000);
 }
